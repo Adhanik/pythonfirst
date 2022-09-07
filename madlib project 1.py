eca80eef907c975bf0885028d80174b958f22df1
Amit Dhanik <amitdhanik3@gmail.com>
2022-09-07 21:31:03 +0530

    Madlib python project

---
+++ madlib project 1.py
@@ -0,0 +1,7 @@
+adjective= input("Enter a adjective \n")
+Verb1= input("Enter a verb \n")
+Verb2= input("Enter antoher verb \n")
+famous_person= input("Enter name of person person \n")
+
+print(f"Computer programming is so {adjective}! It makes me so excited all the time because \
+I love to {Verb1}. Stay hydrated and {Verb2} like you are {famous_person} ")
